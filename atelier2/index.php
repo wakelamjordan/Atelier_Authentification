5d3c895040b60a05aaa52f74b40ea20b274fd1cc
jordan wakelam <jwakelams@gmail.com>
2024-12-18 14:24:40 +0100

    mend

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,7 +1,5 @@
 <?php
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
-var_dump(getenv('AUTH_SESSION_USERNAME'));
-die;
 session_start();
 
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
@@ -12,15 +10,31 @@ if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === '12345') {
     exit();
 }
 
+// création d'un array avec plusieurs utilisateurs
+$account = [
+    ["username" => "admin", "password" => "12345"],
+    ["username" => "user", "password" => "utilisateur"]
+];
+
 // Gérer la soumission du formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     // Vérification simple du username et de son password.
+
+    foreach ($account as $user) {
+        var_dump($user[0], $user[1]);
+    }
+
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
+
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', '12345', time() + 3600, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+
+        // génération d'un token aléatoire
+        $token = bin2hex(random_bytes(16));
+
+        setcookie('authToken', $token, time() + 3600, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
@@ -31,11 +45,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
 </head>
+
 <body>
     <h1>Atelier authentification par Cookie</h1>
     <h3>La page <a href="page_admin.php">page_admin.php</a> est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
@@ -49,4 +65,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <button type="submit">Se connecter</button>
     </form>
 </body>
+
 </html>
