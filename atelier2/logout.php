b7d2f5c92cd9d26a3c11a1f6bc04e2b214c85505
jordan wakelam <jwakelams@gmail.com>
2024-12-18 15:11:16 +0100

    mend

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,6 +1,6 @@
 <?php
 // Supprime le cookie d'authentification
-setcookie('authToken', '', time() - 3600, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
+setcookie('authToken', '', time() - 60, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
 
 // Redirection vers la page de connexion
 header('Location: index.php');
