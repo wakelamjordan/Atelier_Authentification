1f8bfafcc66f948ff575b52d1ff9d8bf0a63c93d
jordan wakelam <jwakelams@gmail.com>
2024-12-20 13:00:22 +0100

    atelier 4 avec filtrage resultat selon aut http

--- atelier4/index.php
+++ atelier4/index.php
@@ -1,9 +1,16 @@
 <?php
 // Nom d'utilisateur et mot de passe corrects
-$valid_username = 'admin';
-$valid_password = 'secret';
 
-// var_dump($_SERVER);
+$account = [
+    [
+        'username' => 'admin',
+        'password' => 'secret'
+    ],
+    [
+        'username' => 'user',
+        'password' => 'utilisateur'
+    ]
+];
 
 // Vérifier si l'utilisateur a envoyé des identifiants
 if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
@@ -14,29 +21,53 @@ if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
     exit;
 }
 
-// Vérifier les identifiants envoyés
-if ($_SERVER['PHP_AUTH_USER'] !== $valid_username || $_SERVER['PHP_AUTH_PW'] !== $valid_password) {
-    // Si les identifiants sont incorrects
+foreach ($account as $user) {
+    // $valid_username = 'admin';
+    // $valid_password = 'secret';
+    // $valid_username = $user['username'];
+    // $valid_password = $user['password'];
+
+    // var_dump($_SERVER['PHP_AUTH_USER']);
+
+
+    if ($_SERVER['PHP_AUTH_USER'] === $user['username'] && $_SERVER['PHP_AUTH_PW'] === $user['password']) {
+        $authOk = true;
+        break;
+    }
+}
+
+if (!$authOk) {
     header('WWW-Authenticate: Basic realm="Zone Protégée"');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Nom d\'utilisateur ou mot de passe incorrect.';
     exit;
 }
 
-// Si les identifiants sont corrects
+
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page protégée</title>
 </head>
+
 <body>
     <h1>Bienvenue sur la page protégée</h1>
     <p>Ceci est une page protégée par une authentification simple via le header HTTP</p>
     <p>C'est le serveur qui vous demande un nom d'utilisateur et un mot de passe via le header WWW-Authenticate</p>
     <p>Aucun système de session ou cookie n'est utilisé pour cet atelier</p>
     <p>Vous êtes connecté en tant que : <?php echo htmlspecialchars($_SERVER['PHP_AUTH_USER']); ?></p>
+    <h2>Si vous êtes admin vous pourrez voir le texte suivant :</h2>
+    <?php
+    if ($_SERVER['PHP_AUTH_USER'] === 'admin') {
+        echo '<p><span style="font-size: large; color:red; font-weight:bold">Vous êtes admin</span> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, architecto?</p>';
+    }
+    ?>
+
 </body>
+
 </html>
